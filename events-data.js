15b7b5592808c486064a1f95c0a92a3d5dcd6ffa
Kade <sawyerkd@mail.uc.edu>
2025-11-19 11:54:13 -0500

    fixed proper backslash, added padding to about page text, underscores instead of spaces for all photots

--- events-data.js
+++ events-data.js
@@ -7,32 +7,32 @@ window.EVENTS = [
     id:'evt-1',
     title:'Meet the Eboard',
     date:'2025-11-13',
-    flyer:'images/Executive Board Photos/Group photos/Executive Board Retreat 2025 1.webp',
-    full:'images/Executive Board Photos/Group photos/Executive Board Retreat 2025 1.webp',
+    flyer:'images/Executive_Board_Photos/Group_photos/Executive_Board_Retreat_2025_1.webp',
+    full:'images/Executive_Board_Photos/Group_photos/Executive_Board_Retreat_2025_1.webp',
     description:'Meet the new executive board and learn more about what we have planned this year!.'
   },
   {
     id:'evt-2',
     title:'Corporate Excursion Informational Session',
     date:'2025-11-20',
-    flyer:'images/Event photos/Coporate Excursion Info 1.webp',
-    full:'images/Event photos/Coporate Excursion Info 1.webp',
+    flyer:'images/Event_photos/Coporate_Excursion_Info_1.webp',
+    full:'images/Event_photos/Coporate_Excursion_Info_1.webp',
     description:'Join us in Clifton Court RM 2130 to learn more about the trip and how you can be part of this exclusive porfessional development experience.'
   },
   {
     id:'evt-3',
     title:'Walking on Business',
     date:'TBA',
-    flyer:'images/Logos/ADVANCE new logo non transparent.webp',
-    full:'images/Logos/ADVANCE new logo non transparent.webp',
+    flyer:'images/Logos/ADVANCE_new_logo_non_transparent.webp',
+    full:'images/Logos/ADVANCE_new_logo_non_transparent.webp',
     description:'A community walk in Burnet Woods to enjoy nature and connect with members. This event is led by Jada!'
   },
   {
     id:'evt-4',
     title:'Corporate Excursion',
     date:'2026-03-14',
-    flyer:'images/Logos/ADVANCE new logo non transparent.webp',
-    full:'images/Logos/ADVANCE new logo non transparent.webp',
+    flyer:'images/Logos/ADVANCE_new_logo_non_transparent.webp',
+    full:'images/Logos/ADVANCE_new_logo_non_transparent.webp',
     description:'Join us for a corporate excursion to explore opportunities in New York City! (Mar 14–21, 2026)'
   }
 
