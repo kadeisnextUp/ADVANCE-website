f7643d51b0c67663baacd02a96119e656f47c3cb
kadeisnextUp <sawyerkd@mail.uc.edu>
2026-07-26 12:44:22 -0400

    Add design tokens, shadcn/ui components, and Sora font

---
+++ lib/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local';
+
+export const sora = localFont({
+  src: '../public/Fonts/Sora-VariableFont_wght.ttf',
+  variable: '--font-sora',
+  display: 'swap',
+});
